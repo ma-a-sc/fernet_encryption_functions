1f8552849dc78a47e2ca6b9922511363c5d0bea3
Mark Scharmann <78019633+ma-a-sc@users.noreply.github.com>
2021-03-22 13:22:46 +0100

    v.1.1

--- fernet_functions.py
+++ fernet_functions.py
@@ -1,16 +1,16 @@
 from cryptography.fernet import Fernet
 
 # Ecrypts the first arguments using the second argument as the key.
-def fernet_info_encrypting(information_to_encrypt, fernet_key):
+def fernet_encrypting(information_to_encrypt, fernet_key):
 
     f = Fernet(fernet_key)
     information_to_encrypt_b = information_to_encrypt.encode()
     encrypted_information = f.encrypt(information_to_encrypt_b)
 
-    return encrypted_string
+    return encrypted_information
 
 # Decrypts the first arguments using the second argument as the key.
-def fernet_info_decrypting(information_to_decrypt, fernet_key):
+def fernet_decrypting(information_to_decrypt, fernet_key):
 
     f = Fernet(fernet_key)
     information_to_decrypt_b = information_to_decrypt.encode()
@@ -23,15 +23,15 @@ def fernet_info_decrypting(information_to_decrypt, fernet_key):
 
 # Decodes the password from the database. Calls first the above writeen 
 # decrypting function and then decodes the bytes into a regular format.
-def password_to_decrypt(pw_database, fernet_key):
-    password_to_decrypt_decrypted = fernet_info_decrypting(pw_database, fernet_key)
-    password_to_decrypt_decrypted_d = password_to_decrypt_decrypted.decode()
+def input_to_decrypt(input_ , fernet_key):
+    input_to_decrypt_decrypted = fernet_decrypting(input_ , fernet_key)
+    input_to_decrypt_decrypted_d = input_to_decrypt_decrypted.decode()
 
-    return password_to_decrypt_decrypted_d
+    return input_to_decrypt_decrypted_d
 # Encodes the input from the user. And then decodes the bytes back into a string
 # so that it can be written into the dictionary.
-def password_to_encrypt(input_user, fernet_key):
-    password_fer = fernet_info_encrypting(input_user, fernet_key)
-    password_fer_encoded = password_fer.decode()
+def input_to_encrypt(input_ , fernet_key):
+    input_fer = fernet_encrypting(input_ , fernet_key)
+    input_fer_encoded = input_fer.decode()
 
-    return password_fer_encoded
+    return input_fer_encoded
